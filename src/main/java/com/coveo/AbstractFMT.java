41f2a996eb21ce2ad109d99cb099315fdc6c97b7
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-05-15 12:44:54 +0200

    Use threadsafe list for concurrent modifications (#52)

--- src/main/java/com/coveo/AbstractFMT.java
+++ src/main/java/com/coveo/AbstractFMT.java
@@ -13,13 +13,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Parameter;
 
 public abstract class AbstractFMT extends AbstractMojo {
@@ -62,7 +62,7 @@ public abstract class AbstractFMT extends AbstractMojo {
   @Parameter(defaultValue = "google", property = "style")
   private String style;
 
-  private List<String> filesProcessed = new ArrayList<String>();
+  private List<String> filesProcessed = new CopyOnWriteArrayList<>();
   private int nonComplyingFiles;
 
   /**
